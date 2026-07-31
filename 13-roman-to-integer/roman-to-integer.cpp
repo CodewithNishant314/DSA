e1eeafd80b27dc0dd7f6bb8452a9374c66285b2b
CodewithNishant314 <10b22nishant@gmail.com>
2026-07-31 22:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (87.23%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+        int Num(char c){
+            if(c=='I'){
+                return 1;
+            }else if(c=='V'){
+                return 5;
+            }else if(c=='X'){
+                return 10;
+            }else if(c=='L'){
+                return 50;
+            }else  if(c=='C'){
+                return 100;
+            }else if(c=='D'){
+                return 500;
+            }else{
+                return 1000;
+            }
+        }
+    int romanToInt(string s) {
+        
+
+
+        int sum=0;
+        int index=0;
+        while(index<s.size()-1){
+            if(Num(s[index])<Num(s[index+1])){
+                sum-= Num(s[index]);
+                index++;
+            }else{
+                sum+= Num(s[index]);
+                index++;
+
+            }
+            
+        }
+
+        sum+= Num(s[s.size()-1]);
+
+        return sum;
+        
+    }
+};
