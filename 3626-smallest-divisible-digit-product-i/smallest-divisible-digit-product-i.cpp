c08e3b1bec0905f6affe1cba0ada3d97b5f0ff9f
CodewithNishant314 <10b22nishant@gmail.com>
2026-08-06 23:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (97.62%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while(true){
+           
+            int num=n;
+            int product=1;
+            while(num>0){
+                int digit=num%10;
+                if(digit==0){
+                    return n;
+                }
+                product*=digit;
+                num=num/10;
+            }
+            if(product%t==0){
+                return n;
+            }else{
+                n++;
+                
+            }
+        }
+        
+    }
+};
